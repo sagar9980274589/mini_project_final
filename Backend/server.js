2173702527e6ec2505a4314f032107945ca2ab5e
sagar9980274589 <45660642+sagar9980274589@users.noreply.github.com>
2024-12-08 19:13:18 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://mini-project-final-n5wv-frontend.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 app.use('/api', ordersRouter);
 
